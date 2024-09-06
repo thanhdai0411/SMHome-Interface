c2741a278491413b55acef12011d0b4bfdec452b
thanhdai0411 <ngothanhdai0411@gmail.com>
2024-09-06 21:55:35 +0700

    feat(page): init ui, sidebar

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from "@/providers/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Smart Home",
+    description: "Smart home ui",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={inter.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
